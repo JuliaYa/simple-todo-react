afc1b40341779f0f4b6db0e24407b38b8b13564b
Julia Yakutova <julia.yakutova@gmail.com>
2021-02-12 18:31:19 +0300

    Move components to separate files.

---
+++ src/components/todo-list-item.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const TodoListItem = () => {
+  return (
+    <>
+      Drink Coffee
+    </>
+  );
+};
+
+export default TodoListItem;
--- src/index.js
+++ src/index.js
@@ -1,33 +1,10 @@
 import React from 'react';
 import ReactDom from 'react-dom';
 
-const TodoList = () => {
-  return (
-    <ul>
-      <li>Learn React</li>
-      <li>Build React App</li>
-    </ul>
-  );
-};
+import TodoList from './components/todo-list';
+import SearchPanel from './components/search-panel';
+import AppHeader from './components/app-header';
 
-const AppHeader = () => {
-  return (
-    <h1>My ToDo List</h1>
-  );
-};
-
-const SearchPanel = () => {
-  const searchText = 'Type here to search';
-  const searchStyle = {
-    fontSize: '18px',
-  };
-
-  return (
-    <input 
-      placeholder={ searchText }
-      style={ searchStyle } />
-  );
-};
 
 const App = () => {
   return (
