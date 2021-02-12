ffd693910624e17d5cfe9530f599fcde3affdc5f
Julia Yakutova <julia.yakutova@gmail.com>
2021-02-12 17:51:37 +0300

    Add simple todo.

--- src/index.js
+++ src/index.js
@@ -1,6 +1,15 @@
 import React from 'react';
 import ReactDom from 'react-dom';
 
-const element = <h1>Hello World</h1>;
+const element = (
+  <>
+    <h1>My ToDo List</h1>
+    <input placeholder='Search' />
+    <ul>
+      <li>Learn React</li>
+      <li>Build React App</li>
+    </ul>
+  </>
+  );
 
 ReactDom.render(element, document.getElementById('root'));
