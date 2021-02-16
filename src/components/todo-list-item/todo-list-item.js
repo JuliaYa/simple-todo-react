f64be801e182e0619fe9ca369610db1a91db2225
Julia Yakutova <julia.yakutova@gmail.com>
2021-02-16 19:00:12 +0300

    Mark item as done.

--- src/components/todo-list-item/todo-list-item.js
+++ src/components/todo-list-item/todo-list-item.js
@@ -3,9 +3,25 @@ import React, { Component } from 'react';
 import './todo-list-item.css';
 
 export default class TodoListItem extends Component {
-
+  
+  state = {
+    done: false
+  }
+  
+  onLabelClick() {
+    this.setState({
+      done: true
+    });
+  };
+  
   render() {
     const { label, important = false } = this.props;
+    const { done } = this.state;
+
+    let classNames = 'todo-list-item';
+    if (done) {
+      classNames += ' done';
+    }
 
     const style = {
       color: important ? 'steelblue' : 'black',
@@ -13,11 +29,12 @@ export default class TodoListItem extends Component {
     };
   
     return (
-      <span className="todo-list-item">
+      <span className={ classNames }>
         <span
           className="todo-list-item-label"
-          style={style}>
-          {label}
+          style={ style }
+          onClick={ this.onLabelClick.bind(this) } >
+          { label }
         </span>
   
         <button type="button"
