f16b3593bf01fef60d4aa4db5a0da8a6a458bc38
Julia Yakutova <julia.yakutova@gmail.com>
2021-02-18 16:50:56 +0300

    Add form to add new todo items.

--- src/components/add-new-item-form/add-new-item-form.js
+++ src/components/add-new-item-form/add-new-item-form.js
@@ -1,20 +1,41 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import './add-new-item-form.css';
 
-const AddNewItemForm = (props) => {
-  const { onItemAdded } = props;
+export default class AddNewItemForm extends Component {
+  state = {
+    label: ''
+  };
+  
+  onLabelChange = (e) => {
+    this.setState({
+      label: e.target.value
+    });
+  };
 
-  return (
-    <div className="add-new-item">
-      <span>New todo</span>
-      <button type="button"
-              className="btn btn-outline-secondary btn-sm float-right"
-              onClick={() => onItemAdded('New todo') }>
-        <i className="fa fa-plus" />
-      </button>
-    </div>
-  );
+  onSubmit = (e) => {
+    e.preventDefault();
+    this.props.onItemAdded(this.state.label);
+    this.setState({
+      label: ''
+    });
+  };
+
+  render() {
+    return (
+      <form className="add-new-item-form d-flex"
+            onSubmit={ this.onSubmit }>
+        <input type="text"
+              className="form-control"
+              onChange={ this.onLabelChange }
+              placeholder="Thing you need to do"
+              value={ this.state.label }/>
+        <button type="button"
+                className="btn btn-outline-secondary btn-sm float-right"
+                onClick={ this.onSubmit }>
+          <i className="fa fa-plus" />
+        </button>
+      </form>
+    );
+  };
 }
-
-export default AddNewItemForm;
